8a027e0679b6f25bb1936543bfec77bde3a5fc35
timonson <timo.rossa@tutanota.com>
2023-09-04 04:37:23 +0200

    Add createUrl

---
+++ url.js
@@ -0,0 +1,30 @@
+/**
+ * createUrl.
+ * ```js
+ * const url = new URL("file:///device/abc/xyc/index.html");
+ * console.log(new URL("file.ts", url).href); // file:///device/abc/xyc/file.ts
+ * console.log(new URL("./file.ts", url).href); // file:///device/abc/xyc/file.ts
+ * console.log(new URL("../file.ts", url).href); // file:///device/abc/file.ts
+ * console.log(new URL("../../file.ts", url).href); // file:///device/file.ts
+ * console.log(new URL("../../../file.ts", url).href); // file:///file.ts
+ * console.log(new URL("../../../../file.ts", url).href); // file:///file.ts
+ * console.log(new URL("/file.ts", url).href); // file:///file.ts
+ * console.log(new URL("fia:e//device", url)); // fia:e//device
+ * ```
+ *
+ * @param {string | URL} url
+ * @param {string | URL} [base]
+ */
+export function createUrl(url, base) {
+  return new URL(url, base);
+}
+
+/**
+ * createUrlWith.
+ *
+ * @param {string | URL} base
+ * @returns {(url: string | URL) => URL}
+ */
+export function createUrlWith(base) {
+  return (url) => new URL(url, base);
+}
